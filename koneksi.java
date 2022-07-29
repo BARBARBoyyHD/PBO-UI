2c15254ffae94d7ac6f84397d099e35eeb742d65
BARBARBoyyHD <68151974+BARBARBoyyHD@users.noreply.github.com>
2022-07-30 01:19:55 +0700

    commit

---
+++ koneksi.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package kemahasiswaan_m.nahrul.hayat_10121074;
+import java.io.FileInputStream;
+import java.util.Properties;
+import javax.swing.JOptionPane;
+
+
+/**
+ *
+ * @author dm
+ */
+public class koneksi {
+    public Properties mypanel,myLanguage;
+    private String strNamePanel;
+    public koneksi()
+    {
+        
+    }
+    public String SettingPanel(String nmPanel)
+    {
+        try
+        {
+            mypanel = new Properties();
+            mypanel.load(new FileInputStream
+                        ("lib/database.ini"));
+            strNamePanel = mypanel.getProperty(nmPanel);
+        }
+        catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(null,e.getMessage(),"Error"
+                    ,JOptionPane.INFORMATION_MESSAGE);
+            System.err.println(e.getMessage());
+            System.exit(0);
+        }
+        return strNamePanel;
+                
+    }
+    
+}
